9dac5b5d65b81c6e19ba9ce74499c0ada979212a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-07 23:49:35 +0400

    Add User entity

---
+++ src/main/java/com/company/springbootrabbitmq/controller/MessageJsonController.java
@@ -0,0 +1,27 @@
+package com.company.springbootrabbitmq.controller;
+
+import com.company.springbootrabbitmq.dto.User;
+import com.company.springbootrabbitmq.publisher.RabbitMQJsonProducer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer) {
+        this.jsonProducer = jsonProducer;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user) {
+        jsonProducer.sendMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ..");
+    }
+}
