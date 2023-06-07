1cf835d7a29a65db3d2898947afe8da4800a1c57
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-07 23:54:37 +0400

    Add RabbitMQProducer

--- src/main/java/com/company/springbootrabbitmq/publisher/RabbitMQProducer.java
+++ src/main/java/com/company/springbootrabbitmq/publisher/RabbitMQProducer.java
@@ -15,8 +15,7 @@ public class RabbitMQProducer {
     @Value("${rabbitmq.routing.key}")
     private String routingKey;
 
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(RabbitMQProducer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
 
     private RabbitTemplate rabbitTemplate;
 
