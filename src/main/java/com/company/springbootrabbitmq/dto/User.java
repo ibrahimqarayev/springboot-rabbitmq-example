2a56ddf9d609d820aff648baed1e64ccbfc6e9a6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-07 23:49:48 +0400

    Add User entity

---
+++ src/main/java/com/company/springbootrabbitmq/dto/User.java
@@ -0,0 +1,11 @@
+package com.company.springbootrabbitmq.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+}
